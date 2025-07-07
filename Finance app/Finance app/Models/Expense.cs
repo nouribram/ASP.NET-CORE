099df1c84cb59cd3208bf95a039bc9d046f47c30
nouribram <nourtesla@yahoo.com>
2025-07-07 12:08:45 +0300

    .

--- Finance app/Finance app/Models/Expense.cs
+++ Finance app/Finance app/Models/Expense.cs
@@ -6,7 +6,8 @@ namespace Finance_app.Models
     {
         public int Id { get; set; }
         [Required]
-        public string Description { get; set; } = null!; [Required]
+        public string Description { get; set; } = null!;
+        [Required]
         
         [Range(0.01, double.MaxValue, ErrorMessage ="Amount needs to be higher than 0")]
         public double Amount { get; set; }
