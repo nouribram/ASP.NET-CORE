7fd859465d7b0e0e750535d87f34decb11bbd0c6
nouribram <nourtesla@yahoo.com>
2025-07-08 10:12:03 +0300

    new

--- Finance app/Finance app/Program.cs
+++ Finance app/Finance app/Program.cs
@@ -2,6 +2,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<FinanceAppContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
+
 
 var app = builder.Build();
 
