8cc0bbbb763dae96b4c7ad704124376d10ec4f64
nouribram <nourtesla@yahoo.com>
2025-07-09 12:54:30 +0300

    new

---
+++ financeApp/financeApp/Data/FinanceAppContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using financeApp.Models; 
+
+namespace financeApp.Data
+{
+    public class FinanceAppContext : DbContext
+    {
+        public FinanceAppContext(DbContextOptions<FinanceAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
